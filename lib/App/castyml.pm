d5fbb1bfefc663893afe74752d15b33abbecf05e
Graham Ollis <plicease@cpan.org>
2021-05-11 12:50:16 -0600

    initial implementation

--- lib/App/castyml.pm
+++ lib/App/castyml.pm
@@ -2,10 +2,15 @@ package App::castyml;
 
 use strict;
 use warnings;
-use 5.008004;
+use 5.020;
 
 # ABSTRACT: C-family abstract syntax tree output to YAML
 # VERSION
+
+sub main
+{
+  local (undef, @ARGV) = @_;
+}
 
 1;
 
---
+++ t/castyml.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Test::Script;
+
+script_compiles 'bin/castyml';
+
+done_testing;
+
+
